9facb2b51e8164f7eb1536afbfed7a176d6dc2c8
ShihabZzz <Shihab.riadn@gmail.com>
2020-05-07 20:53:55 -0700

    ssh.sh: Add option to run Private Setup script
    Signed-off-by: ShihabZzz <Shihab.riadn@gmail.com>

--- ssh.sh
+++ ssh.sh
@@ -2,9 +2,22 @@
 #
 # Script to set up ssh connection with Github
 
+# Set Colors
+export TERM=xterm
+red=$(tput setaf 1)
+grn=$(tput setaf 2)
+blu=$(tput setaf 4)
+cya=$(tput setaf 6)
+txtrst=$(tput sgr0)
+
+# Set Build Variables
+echo -ne ${cya}"\nExecute setup script     [y/n]: "${txtrst}
+read setup_script
+echo -e ""
+
 # Installing Curl package
-echo -e "\n================== Installing Curl package ==================\n"
-sudo apt-get install -y curl
+echo -e "\n================== Installing Curl & git package ==================\n"
+sudo apt-get install -y curl git
 
 echo -e "\n================== Setting up ssh connection with GitHub ==================\n"
 read -p "Enter github email : " email
@@ -16,3 +29,11 @@ read -p "Enter github username: " githubuser
 echo "Using username $githubuser"
 read -s -p "Enter github password for user $githubuser: " githubpass
 curl -u "$githubuser:$githubpass" -X POST -d "{\"title\":\"CLOUDVM_`date +%Y%m%d%H%M%S`\",\"key\":\"$pub\"}" https://api.github.com/user/keys
+
+
+# Run Private Script
+if [ "$setup_script" = "y" ];
+  then
+  cd ~ && git clone git@github.com:ShihabZzz/scripts.git && cd scripts && ./setup.sh
+  wait
+fi
