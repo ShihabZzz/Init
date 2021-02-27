2b61983fe346e2916fc2a18e54c339b98f161a57
ShihabZzz <Shihab.riadn@gmail.com>
2021-02-27 21:08:26 +0600

    comment out deprecated git api

--- ssh.sh
+++ ssh.sh
@@ -11,9 +11,9 @@ cya=$(tput setaf 6)
 txtrst=$(tput sgr0)
 
 # Set Build Variables
-echo -ne ${cya}"\nExecute setup script     [y/n]: "${txtrst}
-read setup_script
-echo -e ""
+#echo -ne ${cya}"\nExecute setup script     [y/n]: "${txtrst}
+#read setup_script
+#echo -e ""
 
 # Installing Curl package
 echo -e "\n================== Installing Curl & git package ==================\n"
@@ -23,17 +23,20 @@ echo -e "\n================== Setting up ssh connection with GitHub ============
 read -p "Enter github email : " email
 echo "Using email $email"
 ssh-keygen -t rsa -b 4096 -C "$email"
+eval `ssh-agent -s`
 ssh-add ~/.ssh/id_rsa
-pub=`cat ~/.ssh/id_rsa.pub`
-read -p "Enter github username: " githubuser
-echo "Using username $githubuser"
-read -s -p "Enter github password for user $githubuser: " githubpass
-curl -u "$githubuser:$githubpass" -X POST -d "{\"title\":\"CLOUDVM_`date +%Y%m%d%H%M%S`\",\"key\":\"$pub\"}" https://api.github.com/user/keys
+cat ~/.ssh/id_rsa.pub
+
+#pub=`cat ~/.ssh/id_rsa.pub`
+#read -p "Enter github username: " githubuser
+#echo "Using username $githubuser"
+#read -s -p "Enter github password for user $githubuser: " githubpass
+#curl -u "$githubuser:$githubpass" -X POST -d "{\"title\":\"CLOUDVM_`date +%Y%m%d%H%M%S`\",\"key\":\"$pub\"}" https://api.github.com/user/keys
 
 
 # Run Private Script
-if [ "$setup_script" = "y" ];
-  then
-  cd ~ && git clone git@github.com:ShihabZzz/scripts.git && cd scripts && ./setup.sh
-  wait
-fi
+#if [ "$setup_script" = "y" ];
+  #then
+  #cd ~ && git clone git@github.com:ShihabZzz/scripts.git && cd scripts && ./setup.sh
+  #wait
+#fi
